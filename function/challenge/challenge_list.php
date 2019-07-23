28a01776b7f99688e8ded60c7c4ae02373d50842
LFsWang <tnst92002@gmail.com>
2019-07-23 20:17:17 +0800

    hotfix search

--- function/challenge/challenge_list.php
+++ function/challenge/challenge_list.php
@@ -6,16 +6,31 @@ if (!defined('IN_SKYOJSYSTEM')) {
 
 require_once $_E['ROOT'].'/function/common/pagelist.php';
 use  \SKYOJ\PageList;
+use \SkyOJ\Core\DataBase\DB as DB;
+
 function listHandle()
 {
     global $SkyOJ,$_E;
     $page = $SkyOJ->UriParam(2)??'1';
-    //$uid = \SKYOJ\safe_get('uid')??null;
-    //$pid = \SKYOJ\safe_get('pid')??null;
-    //$result = \SKYOJ\safe_get('result')??null;
+    $uid = \SKYOJ\safe_get('uid')??null;
+    $pid = \SKYOJ\safe_get('pid')??null;
+    $result = \SKYOJ\safe_get('result')??null;
 
     if( !preg_match('/^[1-9][0-9]*$/',$page) )
         $page = '1';
+
+    if( !preg_match('/^[1-9][0-9]*$/',$uid) )
+        $uid = null;
+    if( !preg_match('/^[1-9][0-9]*$/',$pid) )
+        $pid = null;
+    if( !preg_match('/^[1-9][0-9]*$/',$result) )
+        $result = null;
+
+    $query = "";
+
+    if( isset($uid) ) $query .= " AND `uid` = $uid ";
+    if( isset($pid) ) $query .= " AND `pid` = $pid ";
+    if( isset($result) ) $query .= " AND `result` = $result";
 
     $pl = new PageList('challenge');
     $allpage = $pl->all();
@@ -23,7 +38,21 @@ function listHandle()
     $relpage = $allpage - $page + 1;
 
     //$data = $pl->GetPageDataByPage($page,'cid','*','DESC');
-    $data = \SkyOJ\Challenge\Container::loadRange( ($relpage-1)*PageList::ROW_PER_PAGE , $relpage*PageList::ROW_PER_PAGE-1 );
+
+    if( $query !== "" )
+    {
+        $table = DB::tname('challenge');
+        $rows = DB::fetchAllEx("SELECT * FROM `{$table}` WHERE 1=1 $query");
+        $data = [];
+        foreach( $rows as $row )
+        {
+            $p = new \SkyOJ\Challenge\Container(null);
+            if( $p->loadByData($row) );
+                $data[] = $p;
+        }
+    }
+    else
+        $data = \SkyOJ\Challenge\Container::loadRange( ($relpage-1)*PageList::ROW_PER_PAGE , $relpage*PageList::ROW_PER_PAGE-1 );
 
     //LOG::msg(Level::Debug, '', $data);
     $_E['template']['challenge_list_pagelist'] = $pl;
